788245196f79052bc0227a89cd082ef069ac3e60
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2024-10-19 23:56:20 +0530

    Node.js Server 1: Module Types, CommonJS,  ES6 Modules, npm

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const http = require('http')
+// CommonJS Module
+// const http = require('http')
+// const { sum, difference } = require('./modules/calculation.js')
+
+// ES6 Module
+import http from 'http'
+import { sum, difference } from './modules/calculation.js'
 
 const server = http.createServer((req, res) => {
+  console.log(sum(2, 3))
+  console.log(difference(10, 5))
   res.end('BACKEND :)')
 })
 
---
+++ modules/calculation.js
@@ -0,0 +1,13 @@
+const sum = (num1, num2) => {
+  return num1 + num2
+}
+
+const difference = (num1, num2) => {
+  return num1 - num2
+}
+
+// CommonJS Module
+// module.exports = { sum, difference }
+
+// ES6 Module
+export { sum, difference }
