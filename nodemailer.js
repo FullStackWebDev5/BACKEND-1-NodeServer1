bf665defc8fbb9382003a536cc661e9a58af8856
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2024-10-27 00:02:24 +0530

    Node.js Server: nodemailer, Events in Node.js

---
+++ nodemailer.js
@@ -0,0 +1,60 @@
+const http = require('http')
+const nodemailer = require('nodemailer')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const server = http.createServer(async (req, res) => {
+  if(req.url == '/favicon.ico') {
+    return res.end()
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SENDER_PASSWORD
+      }
+    })
+  
+    await transporter.sendMail({
+      from: process.env.SENDER_EMAIL,
+      to: process.env.RECEIVER_EMAIL,
+      subject: 'Welcome to Node.js',
+      text: 'Node.js® is a free, open-source, cross-platform JavaScript runtime environment that lets developers create servers, web apps, command line tools and scripts.'
+    })
+  } catch(error) {
+    console.log('An error occured while sending email: ', error)
+  }
+
+  res.end('Mail sent to the user!')
+})
+
+server.listen(3000)
+
+
+
+
+
+
+
+
+
+/* 
+  # Environment Variables:
+    - Install dotenv, Import and invoke config() method
+    - Create .env file, add variables
+      - EMAIL=user@gmail.com
+    - To use these variables in server
+      - process.env.EMAIL
+
+  # Nodemailer
+    - External package that can be used with Node.js to send emails
+    - Emails can be sent using SMTP (Simple Mail Transfer Protocol)
+
+    - Note:
+      - If service is 'Gmail', create application password: https://myaccount.google.com/apppasswords
+        - Enter the name of you app, something like 'Nodemailer test'
+        - Click 'Create': You will see a modal with 16 character password
+          - This password needs to be used instead of actual Gmail password
+*/
